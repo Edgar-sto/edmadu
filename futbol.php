2c994df7d10b1276d6b81bac76d235bc557f7ace
Edgar-sto <72636802+Edgar-sto@users.noreply.github.com>
2022-05-01 21:02:07 -0500

    .

---
+++ futbol.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'views/parte_superior.php';
+?>
+<!-- Start content-wrapper-->
+<div class="content-wrapper">
+	<div class="container-fluid">
+		<div class="row mt-3">
+			<div class="col-lg-12">
+				<div class="card">
+					<div class="card-body">
+					<div class="card-title"><h4 class="h4">Reporte Telefonía</h4></div>
+						<!-- <div class="row"> -->
+						<iframe width="1024" height="820" frameborder="0" scrolling="no" src="https://fcstats.com/get-table,63,es.php"></iframe>
+						<!-- </div> -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?php
+require_once  'views/parte_inferior.php';
+?>
--- views/parte_superior.php
+++ views/parte_superior.php
@@ -194,6 +194,19 @@
 					  </div>
 				</li>
 				<li>
+					<a href="futbol.php">
+					<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ball-football" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+						<circle cx="12" cy="12" r="9" />
+						<path d="M12 7l4.76 3.45l-1.76 5.55h-6l-1.76 -5.55z" />
+						<path d="M12 7v-4m3 13l2.5 3m-.74 -8.55l3.74 -1.45m-11.44 7.05l-2.56 2.95m.74 -8.55l-3.74 -1.45" />
+					</svg>
+					<span>Futbol</span>
+						<small class="badge float-right badge-light">Liga MX</small>
+					</a>
+				</li>
+
+				<li>
 					<a href="profile.php">
 						<i class="zmdi zmdi-face"></i> <span>Profile</span>
 					</a>
