c448cfffe96d625d5823aad47fdb78bae9cd70d5
Edgar-sto <72636802+Edgar-sto@users.noreply.github.com>
2022-11-17 17:57:51 -0600

    Update GeneradorDePass.php

--- class/GeneradorDePass.php
+++ class/GeneradorDePass.php
@@ -10,7 +10,8 @@ class GeneradorDePass {
     public function generatePasswordLevelSeven()
     {
         $key = "";
-        $pattern = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	$pattern = "qpbd";
+        //$pattern = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         //$pattern = "1234567890abcdefghijklmñnopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ.-_*/=[]{}#@|~¬&()?¿";
         $max = strlen($pattern)-1;
         for($i = 0; $i < $this->tamanio; $i++){
